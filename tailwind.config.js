1215296b1d58aca22a7e41af95c2e01186ea20ef
Arecio Canton <cuba289@gmail.com>
2021-07-21 00:27:15 -0400

    started working on header css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // New compiler
+  mode: 'jit',
+  // Lets tailwind know what pages to care about
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
