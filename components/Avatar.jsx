3ec9e952f2efa44573f59c37a43868346aace761
Arecio Canton <cuba289@gmail.com>
2021-07-27 09:34:44 -0400

    deploy

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -6,7 +6,7 @@ function Avatar({url, className}) {
         src={url}
         alt="profile picture"
         className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
-        />
+        ></img>
     )
 }
 
